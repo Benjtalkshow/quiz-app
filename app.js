6c2b7beae31697ce79db58718224158e3da96839
Benjtalkshow <chinedubenj@gmail.com>
2024-09-25 15:58:44 +0100

    my first commit

---
+++ app.js
@@ -0,0 +1,38 @@
+// variable
+// var a = 12;
+// var b = 4;
+// var c = a + b;
+// console.log(c);
+/////////////////////////
+
+// var a = 6;
+// var b = 4;
+// var c = 2;
+// var d = a + b / c;
+// console.log(d);
+
+///////////////////////
+// var g = 7;
+// var c = 12;
+// var d = g / c;
+// console.log(d);
+
+//////////////////////
+// // numerator
+// var a = 5;
+// var k = a * a;
+// var c = 3;
+// // denominator
+// var y = 2;
+// var g = k - c;
+// var m = y * g;
+// var d = g / m;
+// console.log(d);
+
+/////////////////////
+var p = 5;
+var m = 10;
+var d = p/m
+console.log(d);
+
+document.querySelector('h1').innerHTML += d
